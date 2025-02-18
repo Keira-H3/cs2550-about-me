ea439950b7123573b6a9c5fb9dcac9a6d5fa977b
Keira Halbrook <email@example.com>
2025-02-18 16:00:05 -0700

    day 12??

---
+++ firstjava.js
@@ -0,0 +1,16 @@
+let a = [5, 2, 7, 4, 1];
+console.log(a[2]);
+
+a.push(3);
+console.log(a);
+let x = a.pop();
+console.log(a);
+a.unshift(3);
+a.shift(a);
+//array.splice(startingindex, numofElem2del, [values2add...])
+
+function funcname(a, b, c) {
+    return a;
+}
+
+console.log(funcname(1,2,3));
