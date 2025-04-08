3fd37b98a0cfaa48a19913f8b9578098545eca8e
Keira Halbrook <email@example.com>
2025-04-08 15:36:38 -0600

    18

--- firstjava.js
+++ firstjava.js
@@ -37,3 +37,58 @@ book = {
 };
 console.log(book);
 console.log(book.name)
+
+
+let re = new RegExp("ab", "i");
+//let re = /ab/i;
+let mytext = "hello";
+re.test(mytext);
+
+let words = ["ban", "babble", "make", "flab"];
+for (w of words) {
+    if (re.test(w)) {
+        console.log(w + " matches.")
+    }
+}
+
+let text = "Is this all there is?";
+re = /^Is/;
+re = /is\?$/;
+re = /t.*e/i;
+re = /t.?e/i;
+re = /t.+e/i;
+re = /t.e/i;
+re = /t\We/i;
+re = /[^t]h|e/i;
+re = /a-e/i;
+re.test(text);
+
+
+
+let myRe = /ab*/g;
+let str = "abbcdefabh";
+//myRe.exec(str);
+//myRe.exec(str);
+let myarray = myRe.exec(str);
+while (myarray != null) {
+    console.log(`Found ${myarray[0]}`);
+    myarray = myRe.exec(str);
+}
+
+
+
+
+
+let reEvent = /(B.+)'s (.+day)/;
+let eventStr = "When is Becky's birthday";
+
+let result = re.exec(eventStr);
+console.log(result);
+console.log(`Who's event? ${result[1]}`);
+console.log(`What event? ${result[2]}`);
+
+
+//\(\d{3}\)\d{3}-\d{4}
+let rePhone = /\(\d{3}\)/;
+let myPhone = "mobile phone: (801)863-8000";
+
